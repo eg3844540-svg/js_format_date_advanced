2610ab774f2db0a5c40942ab842282706cff99c4
Erica <eg3844540@gmail.com>
2026-04-25 20:47:49 -0300

    fix: formatDate function

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,28 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
-}
+  const fromSeparators = fromFormat = fromFormat[3];
+  const toSeparators = toFormat[3];
 
-module.exports = formatDate;
+  const dateParts = date.split(fromSeparators);
+
+  const  dateMap = { };
+
+  for (let i = 0; i < 3; i++) {
+    dateMap[fromFormat[i]] = dateParts[i];
+  }
+
+  if (dateMap['yyy'] && !dateMap['yy']) {
+    dateMap['yy'] = dateMap['yyy'].slice(-2);
+  }
+
+  if (dateMap['yy'] && !dateMap['yyy']) {
+     const year = Number(dateMap['yy']);
+     dateMap['yyy'] = year < 30 ? '20' + dateMap['yy'] : '19' + dateMap['yy'];
+  }
+
+  const result = toFormat.slice(0, 3).map(part => dateMap[part]);
+
+  return result.join(toSeparators);
+}
+    module.exports = formatDate;
