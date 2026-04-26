20a702c9b672053efc7102213854af2554fd7712
Erica <eg3844540@gmail.com>
2026-04-25 21:18:57 -0300

    fix: normalize year conversion

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,26 +8,28 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  const fromSeparators = fromFormat[3];
-  const toSeparators = toFormat[3];
+  const fromSeparator = fromFormat[3];
+  const toSeparator = toFormat[3];
 
-  const dateParts = date.split(fromSeparators);
-  const  dateMap = {};
+  const dateParts = date.split(fromSeparator);
+
+  const dateMap = {};
 
   for (let i = 0; i < 3; i++) {
     dateMap[fromFormat[i]] = dateParts[i];
   }
 
-  if (dateMap['YYY']) {
-    dateMap['YY'] = dateMap['YYY'].slice(-2);
+  if (dateMap['YYYY']) {
+    dateMap.year = dateMap['YYYY'];
+  } else if (dateMap['YY']) {
+    const year = Number(dateMap['YY']);
+    dateMap.year = year < 30
+      ? '20' + dateMap['YY']
+      : '19' + dateMap['YY'];
   }
 
-  if (dateMap['YY']) {
-     const year = Number(dateMap['YY']);
-     dateMap['YYY'] = year < 30
-     ? '20' + dateMap['YY']
-     : '19' + dateMap['YY'];
-  }
+  dateMap['YYYY'] = dateMap.year;
+  dateMap['YY'] = dateMap.year.slice(-2);
 
   const result = [];
 
@@ -35,6 +37,7 @@ function formatDate(date, fromFormat, toFormat) {
     result.push(dateMap[toFormat[i]]);
   }
 
-  return result.join(toSeparators);
+  return result.join(toSeparator);
 }
-   module.exports = formatDate;
+
+module.exports = formatDate;
